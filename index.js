ada7d71ebfedd92ef12223c90aa3c89900c43c53
Gunther <gunt.valentine@gmail.com>
2022-08-11 16:06:17 -0400

    Fetches information from growstuff.org API

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-let form = document.querySelector("#plant-form")
+let form = document.querySelector("form")
+
+form.addEventListener(`submit`, function(e){
+    e.preventDefault()
+    var search = document.getElementById("search").value 
+    fetch("https://www.growstuff.org/api/v1/crops")
+    .then((resp) => resp.json())
+    .then((data) => {
+        console.log(data.Filter(
+            function(data){ return data.attributes.name == plantName }
+    ))
+        
+
+        });
+        // document.getElementById("plants").innerHTML = `
+        // <p>Plant Name: `
+    })
 
