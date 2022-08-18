0c217e13a67eb8855e563adeed750e66926a0c0d
Gunther <gunt.valentine@gmail.com>
2022-08-18 17:44:41 -0400

    MVP functioning

--- NOTES.md
+++ NOTES.md
@@ -1,5 +1,5 @@
 ### Ideas?
-* Searching for plants and get pictures and growing tips using the https://www.growstuff.org/
+* Save a list of crops you can plan your garden with and get information https://www.growstuff.org/ API
 
 ### Overview
 * User can search for crop and learn more about growing information and tips.
@@ -12,7 +12,8 @@
 * Favorite plants to grow
 
 ### MVP
-* Form to search API for plants
+* Form to add crops
+* List of favorited crops / can delete
 * Plant results are displayed with picture (DCOMContentLoaded)
 * User clicks on picture for more information
 
--- index.js
+++ index.js
@@ -1,18 +1,47 @@
-let form = document.querySelector("form")
+document.addEventListener("DOMContentLoaded", () => {
+    let form = document.querySelector("form")
+    let button = document.querySelector("button")
+    let gardenDiv = document.querySelector("#garden-section")
+    const scienceList = document.getElementById("scienceNames")
+    const description = document.getElementById("description")
+    let handleButton = function (e) {
+        let ul = document.createElement('ul')
+        ul.textContent = `${plantName}`
+        console.log(ul)
+        gardenDiv.append(ul)
+}
 
-form.addEventListener(`submit`, function(e){
-    e.preventDefault()
-    var search = document.getElementById("search").value 
-    fetch("https://www.growstuff.org/api/v1/crops")
-    .then((resp) => resp.json())
-    .then((data) => {
-        console.log(data.Filter(
-            function(data){ return data.attributes.name == plantName }
-    ))
-        
-
-        });
-        // document.getElementById("plants").innerHTML = `
-        // <p>Plant Name: `
+    form.addEventListener(`submit`, function (e) {
+        e.preventDefault()
+        scienceList.innerHTML = ""
+        var search = document.getElementById("search").value.toLowerCase();
+        fetch(`http://www.growstuff.org/crops/` + search + ".json", {
+                header: 'Access-Control-Allow-Origin'
+            })
+            .then((resp) => resp.json())
+            .then((data) => {
+                button.removeEventListener(`click`, handleButton)
+                button.hidden = ""
+                console.log(data)
+                plants = data
+                plantName = plants.openfarm_data.attributes.name
+                plantImage = plants.openfarm_data.attributes.main_image_path
+                plantDescription = plants.openfarm_data.attributes.description
+                console.log(plantName)
+                document.getElementById("plantName").innerHTML = `
+                ${plantName}`
+                document.getElementById("plantPic").src = `
+                ${plantImage}`
+                scienceNames = plants.scientific_names
+                scienceNames.forEach(scienceName => {
+                    let listedScience = document.createElement("li")
+                    listedScience.innerHTML = `
+                    ${scienceName.name}`
+                    console.log(listedScience)
+                    scienceList.append(listedScience)
+                })
+                description.innerHTML = plantDescription
+                button.addEventListener(`click`, handleButton)
+            })
     })
-
+})
